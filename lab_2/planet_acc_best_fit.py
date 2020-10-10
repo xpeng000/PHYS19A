b2dfca06736afba7ed83ebe79fee10cd76d9fc04
Rachel Peng <xpeng@brandeis.edu>
2020-10-10 11:39:44 +0800

    completed lab 2

--- lab_2/planet_acc_best_fit.py
+++ lab_2/planet_acc_best_fit.py
@@ -114,7 +114,7 @@ plt.show()
 
 print()
 print("The acceleration obtained from performing least squares curve fitting for data_1.txt is =  %.2f +/- %.2f m/s^2" %(g_1_1, g_err_1))
-print("The acceleration obtained from performing least squares curve fitting for data_2.txt is =  %.1f +/- %.1f m/s^2" %(g_1_2, g_err_2))
+print("The acceleration obtained from performing least squares curve fitting for data_2.txt is =  %.2f +/- %.2f m/s^2" %(g_1_2, g_err_2))
 print("The acceleration obtained from performing least squares curve fitting for data_3.txt is =  %.2f +/- %.2f m/s^2" %(g_1_3, g_err_3))
 print("The acceleration obtained from performing least squares curve fitting for data_4.txt is =  %.2f +/- %.2f m/s^2" %(g_1_4, g_err_4))
 print("The acceleration obtained from performing least squares curve fitting for data_5.txt is =  %.2f +/- %.2f m/s^2" %(g_1_5, g_err_5))
@@ -123,7 +123,7 @@ print()
 # acceleration weighted average:
 weighted_ave = (g_1_1/g_err_1**2+g_1_2/g_err_2**2+g_1_3/g_err_3**2+g_1_4/g_err_4**2+g_1_5/g_err_5**2)/(1/g_err_1**2+1/g_err_2**2+1/g_err_3**2+1/g_err_4**2+1/g_err_5**2)
 weighted_err = np.sqrt(1/(1/g_err_1**2+1/g_err_2**2+1/g_err_3**2+1/g_err_4**2+1/g_err_5**2))
-print("The weighted average acceleration is %.3f +/- %.3f m/s^2" %(weighted_ave, weighted_err))
+print("The weighted average acceleration is %.2f +/- %.2f m/s^2" %(weighted_ave, weighted_err))
         
 
 # Method 2: Fitting with Linear Regression
@@ -178,31 +178,30 @@ def func_velo(t, a):
 par21, cov21 = curve_fit(func_velo, velVsTime_1[0], velVsTime_1[1])
 g_21 = par21[0]
 g_err_21 = np.sqrt(cov21[0, 0])
-print("The acceleration obtained from performing least squares curve fitting for data_1.txt is =  %.1f +/- %.1f m/s^2" %(g_21, g_err_21))
-print()
+print("The acceleration obtained from performing least squares curve fitting for data_1.txt is =  %.2f +/- %.2f m/s^2" %(g_21, g_err_21))
 
 par22, cov22 = curve_fit(func_velo, velVsTime_2[0], velVsTime_2[1])
 g_22 = par22[0]
 g_err_22 = np.sqrt(cov22[0, 0])
-print("The acceleration obtained from performing least squares curve fitting for data_2.txt is =  %.1f +/- %.1f m/s^2" %(g_22, g_err_22))
-print()
+print("The acceleration obtained from performing least squares curve fitting for data_2.txt is =  %.2f +/- %.2f m/s^2" %(g_22, g_err_22))
+
 
 par23, cov23 = curve_fit(func_velo, velVsTime_3[0], velVsTime_3[1])
 g_23 = par23[0]
 g_err_23 = np.sqrt(cov23[0, 0])
-print("The acceleration obtained from performing least squares curve fitting for data_3.txt is =  %.1f +/- %.1f m/s^2" %(g_23, g_err_23))
-print()
+print("The acceleration obtained from performing least squares curve fitting for data_3.txt is =  %.2f +/- %.2f m/s^2" %(g_23, g_err_23))
+
 
 par24, cov24 = curve_fit(func_velo, velVsTime_4[0], velVsTime_4[1])
 g_24 = par24[0]
 g_err_24 = np.sqrt(cov24[0, 0])
-print("The acceleration obtained from performing least squares curve fitting for data_4.txt is =  %.1f +/- %.1f m/s^2" %(g_24, g_err_24))
-print()
+print("The acceleration obtained from performing least squares curve fitting for data_4.txt is =  %.2f +/- %.2f m/s^2" %(g_24, g_err_24))
+
 
 par25, cov25 = curve_fit(func_velo, velVsTime_5[0], velVsTime_5[1])
 g_25 = par25[0]
 g_err_25 = np.sqrt(cov25[0, 0])
-print("The acceleration obtained from performing least squares curve fitting for data_1.txt is =  %.1f +/- %.1f m/s^2" %(g_25, g_err_25))
+print("The acceleration obtained from performing least squares curve fitting for data_5.txt is =  %.2f +/- %.2f m/s^2" %(g_25, g_err_25))
 print()
 
 # acceleration weighted average:
@@ -210,30 +209,225 @@ weighted_ave_2 = (g_21/g_err_21**2+g_22/g_err_22**2+g_23/g_err_23**2+g_24/g_err_
 weighted_err_2 = np.sqrt(1/(1/g_err_21**2+1/g_err_22**2+1/g_err_23**2+1/g_err_24**2+1/g_err_25**2))
 print("The weighted average acceleration is %.2f +/- %.2f m/s^2" %(weighted_ave_2, weighted_err_2))
 
-
+# question 8
 slope, intercept, r_value, p_value, std_err = stats.linregress(velVsTime_1[0], velVsTime_1[1])
 plt.plot(velVsTime_1[0], velVsTime_1[1], 'o')
 plt.plot(velVsTime_1[0], func_velo(velVsTime_1[0], g_21))
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity (m/s)")
-plt.legend(['Scatter plot','Best fit line g parameter: (%.3f +/- %.3f) m/s^2' %(slope, std_err)])
-plt.title("Time vs Velocity with Linear Regression")
+plt.legend(['Scatter plot','Best fit line g parameter: (%.2f +/- %.2f) m/s^2' %(slope, std_err)])
+plt.title("Time vs Velocity with Linear Regression from data_1.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(velVsTime_2[0], velVsTime_2[1])
+plt.plot(velVsTime_2[0], velVsTime_2[1], 'o')
+plt.plot(velVsTime_2[0], func_velo(velVsTime_2[0], g_22))
+plt.xlabel("Time (s)")
+plt.ylabel("Velocity (m/s)")
+plt.legend(['Scatter plot','Best fit line g parameter: (%.2f +/- %.2f) m/s^2' %(slope, std_err)])
+plt.title("Time vs Velocity with Linear Regression from data_2.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(velVsTime_3[0], velVsTime_3[1])
+plt.plot(velVsTime_3[0], velVsTime_3[1], 'o')
+plt.plot(velVsTime_3[0], func_velo(velVsTime_3[0], g_23))
+plt.xlabel("Time (s)")
+plt.ylabel("Velocity (m/s)")
+plt.legend(['Scatter plot','Best fit line g parameter: (%.2f +/- %.2f) m/s^2' %(slope, std_err)])
+plt.title("Time vs Velocity with Linear Regression from data_3.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(velVsTime_4[0], velVsTime_4[1])
+plt.plot(velVsTime_4[0], velVsTime_4[1], 'o')
+plt.plot(velVsTime_4[0], func_velo(velVsTime_4[0], g_24))
+plt.xlabel("Time (s)")
+plt.ylabel("Velocity (m/s)")
+plt.legend(['Scatter plot','Best fit line g parameter: (%.2f +/- %.2f) m/s^2' %(slope, std_err)])
+plt.title("Time vs Velocity with Linear Regression from data_4.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(velVsTime_5[0], velVsTime_5[1])
+plt.plot(velVsTime_5[0], velVsTime_5[1], 'o')
+plt.plot(velVsTime_5[0], func_velo(velVsTime_5[0], g_25))
+plt.xlabel("Time (s)")
+plt.ylabel("Velocity (m/s)")
+plt.legend(['Scatter plot','Best fit line g parameter: (%.2f +/- %.2f) m/s^2' %(slope, std_err)])
+plt.title("Time vs Velocity with Linear Regression from data_5.txt")
 plt.show()
 
 
 # Method 3: Numerical Differentiation
+# question 9
 accVsTime = np.empty((2, 0), float)
 for i in range(1, len(velVsTime_1[0])-1):
     acc = (velVsTime_1[1][i+1]-velVsTime_1[1][i-1])/(2*deltaT)
     accVsTime = np.append(accVsTime, [[velVsTime_1[0][i]], [acc]], axis = 1)
+print("First 10 accelerations with time from data_1.txt")
 for i in range(10):
-    print("The acceleration at time %.3f seconds is %.3f m/s" %(accVsTime[0][i], accVsTime[1][i]))
+    print("The acceleration at time %.3f seconds is %.3f m/s^2" %(accVsTime[0][i], accVsTime[1][i]))
+print()
 
 plt.plot(accVsTime[0], accVsTime[1])
 plt.xlabel("Time (s)")
 plt.ylabel("Acceleration (m/s^2)")
-plt.title("Time vs Acceleration with Numerical Differentiation")
+plt.title("Time vs Acceleration with Numerical Differentiation from data_1.txt")
 plt.show()
+
+accVsTime_2 = np.empty((2, 0), float)
+for i in range(1, len(velVsTime_2[0])-1):
+    acc = (velVsTime_2[1][i+1]-velVsTime_2[1][i-1])/(2*deltaT)
+    accVsTime_2 = np.append(accVsTime_2, [[velVsTime_2[0][i]], [acc]], axis = 1)
+print("First 10 accelerations with time from data_2.txt")
+for i in range(10):
+    print("The acceleration at time %.3f seconds is %.3f m/s^2" %(accVsTime_2[0][i], accVsTime_2[1][i]))
+print()
+
+plt.plot(accVsTime_2[0], accVsTime_2[1])
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.title("Time vs Acceleration with Numerical Differentiation from data_2.txt")
+plt.show()
+
+
+accVsTime_3 = np.empty((2, 0), float)
+for i in range(1, len(velVsTime_3[0])-1):
+    acc = (velVsTime_3[1][i+1]-velVsTime_3[1][i-1])/(2*deltaT)
+    accVsTime_3 = np.append(accVsTime_3, [[velVsTime_3[0][i]], [acc]], axis = 1)
+print("First 10 accelerations with time from data_3.txt")
+for i in range(10):
+    print("The acceleration at time %.3f seconds is %.3f m/s^2" %(accVsTime_3[0][i], accVsTime_3[1][i]))
+print()
+
+plt.plot(accVsTime_3[0], accVsTime_3[1])
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.title("Time vs Acceleration with Numerical Differentiation from data_3.txt")
+plt.show()
+
+accVsTime_4 = np.empty((2, 0), float)
+for i in range(1, len(velVsTime_4[0])-1):
+    acc = (velVsTime_4[1][i+1]-velVsTime_4[1][i-1])/(2*deltaT)
+    accVsTime_4 = np.append(accVsTime_4, [[velVsTime_4[0][i]], [acc]], axis = 1)
+print("First 10 accelerations with time from data_4.txt")
+for i in range(10):
+    print("The acceleration at time %.3f seconds is %.3f m/s^2" %(accVsTime_4[0][i], accVsTime_4[1][i]))
+print()
+
+plt.plot(accVsTime_4[0], accVsTime_4[1])
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.title("Time vs Acceleration with Numerical Differentiation from data_4.txt")
+plt.show()
+
+accVsTime_5 = np.empty((2, 0), float)
+for i in range(1, len(velVsTime_5[0])-1):
+    acc = (velVsTime_5[1][i+1]-velVsTime_5[1][i-1])/(2*deltaT)
+    accVsTime_5 = np.append(accVsTime_5, [[velVsTime_5[0][i]], [acc]], axis = 1)
+print("First 10 accelerations with time from data_5.txt")
+for i in range(10):
+    print("The acceleration at time %.3f seconds is %.3f m/s^2" %(accVsTime_5[0][i], accVsTime_5[1][i]))
+print()
+
+plt.plot(accVsTime_5[0], accVsTime_5[1])
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.title("Time vs Acceleration with Numerical Differentiation from data_5.txt")
+plt.show()
+
+# question 10
+err1 = np.sqrt(np.var(accVsTime[1]))/np.sqrt(len(accVsTime[1]))
+err2 = np.sqrt(np.var(accVsTime_2[1]))/np.sqrt(len(accVsTime_2[1]))
+err3 = np.sqrt(np.var(accVsTime_3[1]))/np.sqrt(len(accVsTime_3[1]))
+err4 = np.sqrt(np.var(accVsTime_4[1]))/np.sqrt(len(accVsTime_4[1]))
+err5 = np.sqrt(np.var(accVsTime_5[1]))/np.sqrt(len(accVsTime_5[1]))
+
+mean1 = np.mean(accVsTime[1])
+mean2 = np.mean(accVsTime_2[1])
+mean3 = np.mean(accVsTime_3[1])
+mean4 = np.mean(accVsTime_4[1])
+mean5 = np.mean(accVsTime_5[1])
+
+print("The average acceleration in data_1.txt using numerical differentiation is (%.2f +/- %.2f) m/s^2" %(mean1, err1))
+print("The average acceleration in data_2.txt using numerical differentiation is (%.2f +/- %.2f) m/s^2" %(mean2, err2))
+print("The average acceleration in data_3.txt using numerical differentiation is (%.2f +/- %.2f) m/s^2" %(mean3, err3))
+print("The average acceleration in data_4.txt using numerical differentiation is (%.2f +/- %.2f) m/s^2" %(mean4, err4))
+print("The average acceleration in data_5.txt using numerical differentiation is (%.2f +/- %.2f) m/s^2" %(mean5, err5))
+
+# acceleration weighted average:
+weighted_ave_3 = (mean1/err1**2+mean2/err2**2+mean3/err3**2+mean4/err4**2+mean5/err5**2)/(1/(1/err1**2+1/err2**2+1/err3**2+1/err4**2+1/err5**2))
+weighted_err_3 = np.sqrt(1/(1/err1**2+1/err2**2+1/err3**2+1/err4**2+1/err5**2))
+print("The weighted average acceleration is %.2f +/- %.2f m/s^2" %(weighted_ave_3, weighted_err_3))
+
+print(accVsTime_5)
+
+# question 11
+accInt = []
+slope, intercept, r_value, p_value, std_err = stats.linregress(accVsTime[0], accVsTime[1])
+accInt.append(intercept)
+plt.plot(accVsTime[0], accVsTime[1], 'o', label="Scatter plot")
+plt.plot(accVsTime[0], intercept+slope*accVsTime[0], label = "Best fit line: acceleration = %.2f t + %.2f " %(slope, intercept))
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.legend(['Scatter plot', "Best fit line: acceleration = %.2f t + %.2f m/s^2" %(slope, intercept)])
+plt.title("Acceleration vs Time with Linear Regression from data_1.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(accVsTime_2[0], accVsTime_2[1])
+accInt.append(intercept)
+plt.plot(accVsTime_2[0], accVsTime_2[1], 'o', label="Scatter plot")
+plt.plot(accVsTime_2[0], intercept+slope*accVsTime_2[0], label = "Best fit line: acceleration = %.2f t + %.2f " %(slope, intercept))
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.legend(['Scatter plot', "Best fit line: acceleration = %.2f t + %.2f m/s^2" %(slope, intercept)])
+plt.title("Acceleration vs Time with Linear Regression from data_2.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(accVsTime_3[0], accVsTime_3[1])
+accInt.append(intercept)
+plt.plot(accVsTime_3[0], accVsTime_3[1], 'o', label="Scatter plot")
+plt.plot(accVsTime_3[0], intercept+slope*accVsTime_3[0], label = "Best fit line: acceleration = %.2f t + %.2f " %(slope, intercept))
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.legend(['Scatter plot', "Best fit line: acceleration = %.2f t + %.2f m/s^2" %(slope, intercept)])
+plt.title("Acceleration vs Time with Linear Regression from data_3.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(accVsTime_4[0], accVsTime_4[1])
+accInt.append(intercept)
+plt.plot(accVsTime_4[0], accVsTime_4[1], 'o', label="Scatter plot")
+plt.plot(accVsTime_4[0], intercept+slope*accVsTime_4[0], label = "Best fit line: acceleration = %.2f t + %.2f " %(slope, intercept))
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.legend(['Scatter plot', "Best fit line: acceleration = %.2f t + %.2f m/s^2" %(slope, intercept)])
+plt.title("Acceleration vs Time with Linear Regression from data_4.txt")
+plt.show()
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(accVsTime_5[0], accVsTime_5[1])
+accInt.append(intercept)
+plt.plot(accVsTime_5[0], accVsTime_5[1], 'o', label="Scatter plot")
+plt.plot(accVsTime_5[0], intercept+slope*accVsTime_5[0], label = "Best fit line: acceleration = %.2f t + %.2f " %(slope, intercept))
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.legend(['Scatter plot', "Best fit line: acceleration = %.2f t + %.2f m/s^2" %(slope, intercept)])
+plt.title("Acceleration vs Time with Linear Regression from data_5.txt")
+plt.show()
+
+# calculate ave acceleration and its error
+accMean = np.mean(accInt)
+accStd = np.std(accInt)
+accErr = accStd/(5**0.5)
+print("The average acceleration is %.2f +/- %.2f m/s^2" %(accMean, accErr))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
